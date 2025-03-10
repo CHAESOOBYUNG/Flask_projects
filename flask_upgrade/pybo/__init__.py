c374d74df8460660597ce2575d56862b2b0ec0ee
ChaeSoobyung <gensung990@gmail.com>
2025-03-10 15:42:12 +0900

    flask

--- flask_upgrade/pybo/__init__.py
+++ flask_upgrade/pybo/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 import config
 db=SQLAlchemy()
@@ -8,6 +9,7 @@ migrate=Migrate()
 
 def create_app():
     app=Flask(__name__)
+    CORS(app)
     app.config.from_object(config)
 
     #ORM Initialization
--- flask_upgrade/pybo/views/main_views.py
+++ flask_upgrade/pybo/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, render_template
 from werkzeug.utils import redirect
 
 bp = Blueprint('main',__name__, url_prefix='/')
@@ -7,6 +7,10 @@ bp = Blueprint('main',__name__, url_prefix='/')
 def hello_pybo():
     return 'Hello, Pybo!'
 
-@bp.route('/')
+@bp.route('/question')
 def index():
     return redirect(url_for('question._list'))
+
+@bp.route("/")
+def home():
+    return render_template("main.html")  # templates/main.html을 렌더링
